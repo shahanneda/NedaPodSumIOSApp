1c8f6ba410f39e21b5cda6921617f1cc0c41ccac
Shahan Neda <shahan.neda@gmail.com>
2019-12-07 19:29:43 -0800

    Initial Commit

---
+++ NedaPodSum/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  NedaPodSum
+//
+//  Created by Shahan Nedadahandeh on 2019-12-07.
+//  Copyright © 2019 Shahan Nedadahandeh. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NedaPodSum/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  NedaPodSum
+//
+//  Created by Shahan Nedadahandeh on 2019-12-07.
+//  Copyright © 2019 Shahan Nedadahandeh. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
